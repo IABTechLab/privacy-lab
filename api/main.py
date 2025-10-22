49afadca9b306dde81ecfa2f522fbacd5dc4efa2
xdevanshux <devanshu@kodescan.com>
2025-10-22 23:25:58 +0530

    docker file added and build file

--- api/main.py
+++ api/main.py
@@ -252,6 +252,6 @@ def homomorphic_encryption(request: HomomorphicEncryptionRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+    # import uvicorn
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
